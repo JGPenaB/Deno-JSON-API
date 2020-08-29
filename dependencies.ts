d7f9fadf4b84f7937a7bb4188bb99d02f829828a
JGPenaB <jesusgpenab@gmail.com>
2020-08-29 14:54:12 -0400

    Dependencias actualizadas

--- dependencies.ts
+++ dependencies.ts
@@ -1,3 +1,7 @@
-export {Application, isHttpError, Status, Router} from "https://deno.land/x/oak@v5.2.0/mod.ts";
+export { Application,isHttpError,Status,Router } from "https://deno.land/x/oak@v5.2.0/mod.ts"; 
 export { Client } from "https://deno.land/x/postgres/mod.ts";
 export { config } from "https://deno.land/x/dotenv/mod.ts";
+export { makeJwt,setExpiration,Jose,Payload } from "https://deno.land/x/djwt/create.ts";
+export { assertEquals, assertArrayContains } from "https://deno.land/std@0.60.0/testing/asserts.ts";
+import axiod from "https://deno.land/x/axiod/mod.ts";
+export { axiod };
